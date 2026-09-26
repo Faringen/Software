574157de9bfa2eba4580bb8a87bc9e256655ce3c
faringen <melamurlerina@gmail.com>
2026-09-26 08:22:45 +0300

    Close a Lab work 2 and lab 3

--- src/labs/LabWork2/AnimalThread.java
+++ src/labs/LabWork2/AnimalThread.java
@@ -3,12 +3,9 @@ package labs.LabWork2;
 public class AnimalThread extends Thread {
 
     private final String animalName;
-    private int moveCount = 0;
+    private int distance = 0;
 
-    public int getMoveCount() {
-        return moveCount;
-    }
-
+    // Статический флаг, чтобы определить, кто победил первым в первой части задания
     public static volatile boolean winnerDeclared = false;
 
     public AnimalThread(String name, int priority) {
@@ -17,22 +14,30 @@ public class AnimalThread extends Thread {
         this.setPriority(priority);
     }
 
+    public int getDistance() {
+        return distance;
+    }
+
+    public String getAnimalName() {
+        return animalName;
+    }
+
     @Override
     public void run() {
-        System.out.println("Поток " + animalName + " запущен");
-        while (moveCount < 1000) {
-            moveCount += 10;
+        while (distance < 1000) {
+            distance += 10;
             try {
                 Thread.sleep(2);
             } catch (InterruptedException e) {
-                System.out.println(e.getMessage());
+                System.out.println(animalName + " был прерван!");
                 return;
             }
         }
-        synchronized (AnimalThread.class){
+
+        synchronized (AnimalThread.class) {
             if (!winnerDeclared) {
                 winnerDeclared = true;
-                System.out.println("\n🏆 " + animalName + " ПОБЕДИЛ в первом забеге!");
+                System.out.println(animalName + " ПОБЕДИЛ в первом забеге!");
             }
         }
     }
--- src/labs/LabWork2/RabbitAndTurtle.java
+++ src/labs/LabWork2/RabbitAndTurtle.java
@@ -2,52 +2,44 @@ package labs.LabWork2;
 
 public class RabbitAndTurtle {
     public static void main(String[] args) throws InterruptedException {
-        System.out.println("=== Обычные догонялки ===");
+        System.out.println("=== ЧАСТЬ 1: Обычные догонялки ===");
 
-        // Кролик быстрый (высокий приоритет), Черепаха медленная (низкий приоритет)
         AnimalThread rabbit = new AnimalThread("Кролик", Thread.MAX_PRIORITY);
         AnimalThread turtle = new AnimalThread("Черепаха", Thread.MIN_PRIORITY);
 
         rabbit.start();
         turtle.start();
+
         rabbit.join();
         turtle.join();
 
-        rabbit.interrupt();
-        turtle.interrupt();
-
-        System.out.println("Потоки с более высоким приоритетом (Thread.MAX_PRIORITY) получают " +
-                "больше процессорного времени от квантователя ОС, поэтому Кролик побеждает " +
-                "в абсолютном большинстве случаев.");
-
-
-        System.out.println("\n=== Динамическое изменение приоритетов ===");
+        System.out.println("\n=== ЧАСТЬ 2: Динамическое изменение приоритетов ===");
 
         AnimalThread.winnerDeclared = false;
 
-        AnimalThread fastRabbit = new AnimalThread("Кролик", Thread.MAX_PRIORITY);
-        AnimalThread slowTurtle = new AnimalThread("Черепаха", Thread.MIN_PRIORITY);
+        AnimalThread fastRabbit = new AnimalThread("Динамический кролик", Thread.MAX_PRIORITY);
+        AnimalThread slowTurtle = new AnimalThread("Динамическая черепаха", Thread.MIN_PRIORITY);
 
         fastRabbit.start();
         slowTurtle.start();
 
         while (fastRabbit.isAlive() && slowTurtle.isAlive()) {
-            int rabbitDist = fastRabbit.getMoveCount();
-            int turtleDist = slowTurtle.getMoveCount();
+            int rabbitDist = fastRabbit.getDistance();
+            int turtleDist = slowTurtle.getDistance();
 
             System.out.printf("Промежуточный итог -> Кролик: %d м (Приоритет: %d) | Черепаха: %d м (Приоритет: %d)%n",
                     rabbitDist, fastRabbit.getPriority(), turtleDist, slowTurtle.getPriority());
 
             if (rabbitDist - turtleDist > 100) {
                 if (slowTurtle.getPriority() != Thread.MAX_PRIORITY) {
-                    System.out.println(" Черепаха сильно отстает! Включаем турбо-приоритет для Черепахи и замедляем Кролика.");
+                    System.out.println("Черепаха сильно отстает! Включаем турбо-приоритет для Черепахи и замедляем Кролика.");
                     slowTurtle.setPriority(Thread.MAX_PRIORITY);
                     fastRabbit.setPriority(Thread.MIN_PRIORITY);
                 }
             }
             else if (turtleDist > rabbitDist) {
                 if (fastRabbit.getPriority() != Thread.MAX_PRIORITY) {
-                    System.out.println("⚡ Черепаха вырвалась вперед! Кролик включает максимальную скорость.");
+                    System.out.println("Черепаха вырвалась вперед! Кролик включает максимальную скорость.");
                     fastRabbit.setPriority(Thread.MAX_PRIORITY);
                     slowTurtle.setPriority(Thread.MIN_PRIORITY);
                 }
@@ -59,8 +51,6 @@ public class RabbitAndTurtle {
         fastRabbit.join();
         slowTurtle.join();
         System.out.println("Гонка с динамическими приоритетами успешно завершена!");
-
-        fastRabbit.interrupt();
-        slowTurtle.interrupt();
     }
 }
+
